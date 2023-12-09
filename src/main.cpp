90360bf6b2342a44c10fe38bb8f65cd5b828e8dc
Vaibhav Bajaj <bajajv2003@gmail.com>
2023-12-09 18:43:28 +0530

    added comments in main file

--- src/main.cpp
+++ src/main.cpp
@@ -49,83 +49,93 @@ int main()
 	// /* This FEN should have 23 moves to make for black and 38 for white */
 	// std::string FEN_TESTING = "rnbqk1rQ/4bn2/pppp1p2/4R2B/B3p1pp/PPPPPPPP/8/1NB1K1NR";
 
-	// board b = board::from_fen(FEN_TESTING);
-	// move_gen mg = move_gen(&b, true);
-	// std::vector<board::move> num_legal_moves_test_w = mg.all_legal_moves();
-
-	// std::cout << "Number of legal moves for white: " << num_legal_moves_test_w.size() << std::endl;
-
-	// b.move_piece(num_legal_moves_test_w[0]);
-	// mg.change_turn();
-	// std::vector<board::move> num_legal_moves_test_b = mg.all_legal_moves();
-	// std::cout << "FEN after first move: " << b.to_fen() << std::endl;
-	// std::cout << "Number of legal moves for black: " << num_legal_moves_test_b.size() << std::endl;
-
-	// b.undo_move(num_legal_moves_test_w[0], piece::e_VOID);
-	// mg.change_turn();
-	// std::cout << "FEN after undoing first move: " << b.to_fen() << std::endl;
-	// std::cout << "Number of legal moves for white: " << num_legal_moves_test_w.size() << std::endl;
-
-	// std::cout << "Outcome: " << mg.evaluate() << std::endl;
-
 	std::string MINMAX_TEST; 
-	int depth;
-	// MINMAX_TEST = "k/p/P/K";
+	int maxDepth;
 	std::cout << "FEN: ";
 	std::cin >> MINMAX_TEST;
 	std::cout << "Depth: ";
-	std::cin >> depth;
+	std::cin >> maxDepth;
 	std::cout << std::endl;
 
-	std::cout << "White wins if evaluation is between " << 100000 - depth << " to 100000" << std::endl;
-	std::cout << "Black wins if evaluation is between " << -100000 - depth << " to -100000" << std::endl;
-	std::cout << "Draw if evaluation is between " << -depth << " to 0" << std::endl;
+	std::cout << "White wins if evaluation is between " << 100000 - maxDepth << " to 100000" << std::endl;
+	std::cout << "Black wins if evaluation is between " << -100000 - maxDepth << " to -100000" << std::endl;
+	std::cout << "Draw if evaluation is between " << -maxDepth << " to 0" << std::endl;
 	std::cout << std::endl;
 
-	board b2 = board::from_fen(MINMAX_TEST);
-	move_gen mg2 = move_gen(&b2, true);
-	MinMax mm(mg2);
+	board b = board::from_fen(MINMAX_TEST);
+	move_gen mg = move_gen(&b, true);
+	MinMax mm(mg);
 
 	std::cout << "Board: " << std::endl;
-	print_board(b2);
+	print_board(b);
 
-	// int eval = mm.minmaxSimple(b2, depth, true, 0);
+	// ------------------------------------------------------------
+
+	// To test Minmax Simple, uncomment the following lines
+	
+	// int eval = mm.minmaxSimple(b, maxDepth, true, 0);
 	// std::cout << "Evaluation: " << eval << std::endl;
-	// std::cout << mm.result(eval, depth) << std::endl;
+	// std::cout << mm.result(eval, maxDepth) << std::endl;
 	// mm.clearall();
 
+	// ------------------------------------------------------------
+
+	// To test Minmax Alpha-Beta with time, uncomment the following lines
+
 	// std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-	// int eval = mm.minmaxAlphaBeta(b2, depth, true, std::numeric_limits<int>::min(), std::numeric_limits<int>::max(), 0);
+	// int eval = mm.minmaxAlphaBeta(b, maxDepth, true, std::numeric_limits<int>::min(), std::numeric_limits<int>::max(), 0);
 	// std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 	// std::cout << "Evaluation: " << eval << std::endl;
-	// std::cout << mm.result(eval, depth) << std::endl;
+	// std::cout << mm.result(eval, maxDepth) << std::endl;
 	// std::cout << "Depth limit: " << mm.depth_limit << std::endl;
 	// std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[us]" << std::endl;
 	// mm.clearall();
 
-	// std::cout << "Number of nodes: " << mm.getNodes(b2, depth, true, true) << std::endl;
+	// ------------------------------------------------------------
+
+	// To test number of nodes, uncomment the following lines
+
+	// std::cout << "Number of nodes: " << mm.getNodes(b, maxDepth, true, true) << std::endl;
 	// mm.clearall();
 
-	// auto p = mm.comparePruning(b2, depth, true);
+	// ------------------------------------------------------------
+
+	// To test compare pruning, uncomment the following lines
+
+	// auto p = mm.comparePruning(b, maxDepth, true);
 	// std::cout << "Time taken for simple minmax: " << p.first.count() << std::endl;
 	// std::cout << "Time taken for alpha-beta minmax: " << p.second.count() << std::endl;
 	// std::cout << "Ratio: " << p.first.count()/p.second.count() << std::endl;
 	// mm.clearall();
 
-	// auto nodes = mm.getNodesAtDepth(b2, depth, true);
-	// std::cout << "Number of nodes at depth " << depth << ": " << nodes << std::endl;
+	// ------------------------------------------------------------
+
+	// To test getNodesAtDepth, uncomment the following lines
+
+	// auto nodes = mm.getNodesAtDepth(b, maxDepth, true);
+	// std::cout << "Number of nodes at maxDepth " << maxDepth << ": " << nodes << std::endl;
 	// if (mm.isDepthLimitReached())
 	// 	std::cout << "Depth limit reached!" << std::endl;
 	// else
 	// 	std::cout << "Depth limit not reached! All games ended before." << std::endl;
 	
-	mm.optimalPlay(b2, depth, true, true, 100);
+	// ------------------------------------------------------------
 
-	// Depth limit test
+	// To test optimalPlay, uncomment the following lines
+
+	// mm.optimalPlay(b, maxDepth, true, true, 100);
+
+	// ------------------------------------------------------------
+
+	// To test isDepthLimitReached, uncomment the following lines and comment
+	// the mm.clearall() lines in the above tests
+
 	// if (mm.isDepthLimitReached())
 	// 	std::cout << "Depth limit reached!" << std::endl;
 	// else
 	// 	std::cout << "Depth limit not reached!" << std::endl;
+
+	// ------------------------------------------------------------
 
 	return 0;
 };
